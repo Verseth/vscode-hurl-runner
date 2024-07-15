aac4dc29e908bcb596d7b0f9d4270d2a3e3debaf
Mateusz Drewniak <matmg24@gmail.com>
2024-07-15 16:47:42 +0200

    Implement hurl runner

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# vscode-hurl-runner
-Hurl Runner makes it easy to run Hurl files through VSCode.
+# hurl-runner
+
+<img srcset="./resources/hurl-logo.svg" alt="drawing" width="200"/>
+
+
+Hurl Runner makes it easy to run [hurl](https://hurl.dev/) files through VSCode.
+
+
+
+## Features
+
+![Hurl Runner GIF](./resources/hurl-runner.gif)
+
+Hurl Runner adds a handy button that executes the current Hurl file.
+
+![Hurl run](./resources/hurl-run.png)
+
+It adds an environment management system similar to Postman.
+The current env is displayed at the bottom as a status bar item.
+
+![Hurl env](./resources/hurl-env.png)
+
+You can change the current env by clicking on it.
+
+![Hurl env choice](./resources/hurl-env-choice.png)
+
+By default environment files are looked for under `./hurl/env` in your workspace.
+You can change it in extension settings.
+
+![Hurl env dir](./resources/hurl-env-dir.png)
+
+Hurl Runner will pass the currently active environment file to hurl using the [--variables-file](https://hurl.dev/docs/manual.html#variables-file) option.
+
+## Requirements
+
+[Hurl](https://hurl.dev/) has to be installed on your system for the extension to work properly.
---
+++ src/extension.ts
@@ -0,0 +1,128 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { isAbsolute } from 'node:path';
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	let envButton: vscode.StatusBarItem;
+	const noEnv = "no env";
+
+	const refreshChooseEnvButton = () => {
+		let currentEnvFile: string | undefined = context.workspaceState.get("hurl-runner.envFile");
+		if (currentEnvFile) {
+			envButton.text = `$(key) Hurl: ${currentEnvFile}`;
+		} else {
+			envButton.text = `$(key) Hurl: ${noEnv}`;
+		}
+		envButton.show();
+	};
+
+	const getFullEnvFilePath = (): string | undefined => {
+		if(!vscode.workspace.workspaceFolders) {
+			return;
+		}
+		let currentEnvFile: string | undefined = context.workspaceState.get("hurl-runner.envFile");
+		if (!currentEnvFile) {
+			return;
+		}
+
+		let config = vscode.workspace.getConfiguration('hurl-runner');
+		let envDir: string | undefined = config.envDir;
+
+		if (!envDir) {
+			return;
+		}
+
+		let envDirUri: vscode.Uri;
+		if (!isAbsolute(envDir)) {
+			let workspace = vscode.workspace.workspaceFolders[0];
+			envDirUri = vscode.Uri.joinPath(workspace.uri, envDir);
+		} else {
+			envDirUri = vscode.Uri.parse(envDir);
+		}
+
+		let envFileUri = vscode.Uri.joinPath(envDirUri, currentEnvFile);
+		return envFileUri.fsPath;
+	};
+
+	if(vscode.workspace.workspaceFolders) {
+		envButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 3);
+		envButton.command = 'hurl-runner.chooseEnv';
+
+		context.subscriptions.push(envButton);
+		refreshChooseEnvButton();
+	}
+
+	const runFileCommand = vscode.commands.registerCommand('hurl-runner.runFile', () => {
+		let terminal = vscode.window.activeTerminal || vscode.window.createTerminal();
+		let editor = vscode.window.activeTextEditor;
+    if(!editor || !editor.document || !editor.document.fileName || editor.document.languageId !== 'hurl') {
+			return;
+		}
+
+		let currentEnvFile = getFullEnvFilePath();
+
+		let fileName = editor.document.fileName;
+		let config = vscode.workspace.getConfiguration('hurl-runner');
+		let commandLineArguments: string = config.commandLineArguments;
+
+		let command = `hurl ${fileName} ${commandLineArguments}`;
+		if (currentEnvFile) {
+			command += ` --variables-file ${currentEnvFile}`;
+		}
+
+		terminal.show();
+		terminal.sendText(command);
+	});
+	context.subscriptions.push(runFileCommand);
+
+	const chooseEnvCommand = vscode.commands.registerCommand('hurl-runner.chooseEnv', async () => {
+		let config = vscode.workspace.getConfiguration('hurl-runner');
+		let envDir: string | undefined = config.envDir;
+		if(!vscode.workspace.workspaceFolders) {
+			vscode.window.showErrorMessage("You must be in a workspace to use hurl environments");
+			return;
+		}
+		if (!envDir) {
+			vscode.window.showErrorMessage("You should configure hurl-runner.envDir");
+			return;
+		}
+
+		let envDirUri: vscode.Uri;
+		if (!isAbsolute(envDir)) {
+			let workspace = vscode.workspace.workspaceFolders[0];
+			envDirUri = vscode.Uri.joinPath(workspace.uri, envDir);
+		} else {
+			envDirUri = vscode.Uri.parse(envDir);
+		}
+		let envNames: string[];
+
+		try {
+			let envs = await vscode.workspace.fs.readDirectory(envDirUri);
+			envNames = envs.map(([name, _]) => name);
+		} catch {
+			vscode.window.showErrorMessage(`There are no defined environments under ${envDir}`);
+			return;
+		}
+
+		envNames.push(noEnv);
+		let selection = await vscode.window.showQuickPick(envNames);
+		if (!selection) {
+			return;
+		}
+		if (selection === noEnv) {
+			context.workspaceState.update("hurl-runner.envFile", undefined);
+			refreshChooseEnvButton();
+			return;
+		}
+
+		context.workspaceState.update("hurl-runner.envFile", selection);
+		refreshChooseEnvButton();
+	});
+	context.subscriptions.push(chooseEnvCommand);
+}
+
+// This method is called when your extension is deactivated
+export function deactivate() {}
